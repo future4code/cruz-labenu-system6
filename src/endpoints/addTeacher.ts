ad274f7816ab7ff439bcb69853bbb071601de97a
laisdejesus <dejesusll@outlook.com>
2021-06-02 16:53:13 -0300

    Add estudante a turma

---
+++ src/endpoints/addTeacher.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import connection from "../connection";
+import dayjs from "dayjs";
+
+export const addTeacher = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        let { name, email, birthDate } = req.body
+
+        if(!name || !email || !birthDate) {
+            throw new Error("Missing parameters. name, email and birthDate(YYYY-MM-DD) are required.")
+        }
+
+        if(!dayjs(birthDate).isValid()) {
+            throw new Error("Invalid date.")
+        }
+
+        await connection("teacher")
+                .insert({name, email, birth_date: birthDate})
+        res.status(201).send("Created teacher")
+
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    }
+} 
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import app from "./app";
 import { createStudent } from "./endpoints/createStudent";
 import { getStudents } from "./endpoints/getStudents";
 import { createTeacher } from "./endpoints/createTeacher";
+import { addStudentToClass } from "./endpoints/addStudentToClass";
 
 
 app.get("/students/all", getStudents);
@@ -9,3 +10,5 @@ app.get("/students/all", getStudents);
 app.post('/student', createStudent);
 
 app.post("/teacher", createTeacher);
+
+app.post("/students-class/:id", addStudentToClass);
