6d3c6b02825d19f2c586adcf0781d4c72e28e1e5
laisdejesus <dejesusll@outlook.com>
2021-06-02 15:35:52 -0300

    Endpoint de criar professor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import app from "./app";
 import { createStudent } from "./endpoints/createStudent";
 import { getStudents } from "./endpoints/getStudents";
+import { createTeacher } from "./endpoints/createTeacher";
 
 
 app.get("/students/all", getStudents);
 
-app.post('/student', createStudent)
+app.post('/student', createStudent);
+
+app.post("/teacher", createTeacher);
