123c91e9a7612f0449e2a9c6e924bf98bf1c45b5
Luana <lualuna182@gmail.com>
2021-06-02 16:53:53 -0300

    add professor a turma ok

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import app from "./app";
 import { createClass } from "./endpoints/createClass";
 import { createStudent } from "./endpoints/createStudent";
 import { getStudents } from "./endpoints/getStudents";
+import { addTeacherToClass } from './endpoints/addTeacherToClass'
 
 
 app.get("/students/all", getStudents);
@@ -9,3 +10,5 @@ app.get("/students/all", getStudents);
 app.post('/student', createStudent)
 
 app.post('/createClass', createClass)
+
+app.post('/teacher/:teacherId', addTeacherToClass)
